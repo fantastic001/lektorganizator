ea8509f50287fca9578b91bed4f3ae3e29759055
Stefan Nožinić <stefan@lugons.org>
2016-05-03 14:16:56 +0200

    Show real description in admin panel

--- lektorganizator/app/models.py
+++ lektorganizator/app/models.py
@@ -6,9 +6,15 @@ class Lecturer(models.Model):
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
 
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
 
 class Article(models.Model):
     slug = models.SlugField(allow_unicode=True) 
     archived = models.BooleanField(default=False) 
     lecturer = models.ForeignKey(Lecturer, blank=True, null=True)
     notified = models.BooleanField(default=False) 
+
+    def __str__(self):
+        return self.slug
